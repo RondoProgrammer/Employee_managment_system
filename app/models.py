0c3163f48c3b69e4dafde08d7c6c59139c1faf3e
Arli Asllani <arliasllani04@gmail.com>
2025-08-06 13:44:26 +0200

    Changed models so admin and manager are in one table which has a role column to differentiate them

--- app/models.py
+++ app/models.py
@@ -3,32 +3,6 @@ from flask_login import UserMixin
 from app import db
 
 
-class Administrator(UserMixin, db.Model):
-    __tablename__ = 'administrators'
-
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(64), unique=True, nullable=False)
-    password = db.Column(db.String(64), nullable=False)
-
-    def __repr__(self):
-        return f'<Administrator {self.name}>'
-    
-
-class DepartmentManager(UserMixin, db.Model):
-    __tablename__ = 'department_managers'
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(64), nullable=False)
-    password = db.Column(db.String(64), nullable=False)
-    
-    #db.relationship sets up a relationship between two model classes in your code — not directly in the database.
-    #unique=True ensures each manager can be assigned to only one department
-    #uselist=False makes the relationship one-to-one from Department side
-    department = db.relationship('Department', backref=db.backref('manager',uselist=False))
-
-    def __repr__(self):
-        return f'<DepartmentManager {self.name}>'
-
-
 class Department(db.Model):
     __tablename__ = 'departments'
 
@@ -61,3 +35,18 @@ class Employee(UserMixin, db.Model):
     #creates a string of the above, helpful when debugging
     def __repr__(self):
         return f'<Employee {self.name}>'
+
+
+class User(UserMixin, db.Model):
+    __tablename__ = 'users'
+
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(64), unique=True, nullable=False)
+    password = db.Column(db.String(64), nullable=False)
+    role = db.Column(db.String(32), nullable=False)  # e.g., 'administrator', 'department_manager', 'employee'
+
+    deparment_id = db.Column(db.Integer, db.ForeignKey('departments.id'), nullable=True)
+    department = db.relationship('Department', backref='manager', uselist=False)
+    
+    def __repr__(self):
+        return f'<User {self.username}>'
